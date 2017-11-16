f20af04e7cbed441dbf665c5520b6dfabec37bb2
onemoon <sopthfg@gmail.com>
2017-11-16 23:38:29 +0900

    nomad coder react lecture  END

--- react_movie_nomad/src/App.js
+++ react_movie_nomad/src/App.js
@@ -8,26 +8,50 @@ class App extends Component {
 
 // 컴포넌트 안에 state가 바뀔때 마다 render가 다시 실행된다.
 
-state = {}
+  state = {}
 
-componentWillMount(){
-  
-}
+  componentWillMount(){
+    this._getMovies();
+  }
 
-componentDidMount(){
-  fetch('https://yts.ag/api/v2/list_movies.json?sort_by=rating');
-}
+  componentDidMount(){
 
-// 언더스코어는 리액트에 함수가 많기 때문에 나의 함수를 구분하기 위해서 사용함.
-_renderMovies = ()=>{
-  const movies = this.state.movies.map((movie,index) => {
-    return <Movie title={movie.title} poster={movie.poster} key={index}/>
-  })
-  return movies; 
-}
+  }
+
+  // 언더스코어는 리액트에 함수가 많기 때문에 나의 함수를 구분하기 위해서 사용함.
+  _renderMovies = ()=>{
+    const movies = this.state.movies.map(movie => {
+      return <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres = {movie.genres} 
+      synopsis = { movie.synopsis}
+      />
+    })
+    return movies;
+  }
+
+  _getMovies = async () =>{
+    const movies = await this._callApi()
+    // await mode -> 끝나기를 기다린다 (성공하기를 기다리는게 아니다.)
+    // async 가 있어야 await 가 작동을 한다.
+    this.setState({
+      movies
+    });
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.ag/api/v2/list_movies.json?sort_by=rating")
+    .then( result => result.json() )
+    .then( json => json.data.movies )
+    .catch(err => console.log(err) )
+
+    // promise - > then을 통해서 결과물이 나오고 catch를 통해서 error 를 잡는다
+    // 지금은 readablestream으로 바디가 나오는데 이거를 json 형태로 바꿔줘야 한다.
+  }
 
   render() {
-    
     return (
       <div className="App">
         {this.state.movies ? this._renderMovies() : "Loading"}
--- react_movie_nomad/src/Movie.js
+++ react_movie_nomad/src/Movie.js
@@ -19,28 +19,52 @@ import './Movie.css';
 //   }
 // }
 
-function Movie({title,poster}){
+function Movie({title,poster,genres,synopsis}){
   return(
-        <div>
-          <MoviePoster poster={poster}/>
-          <h1>{title}</h1>
+        <div className="Movie">
+          <div className = "Movie_Columns">
+            <MoviePoster poster={poster} alt={title}/>
+          </div>
+          <div className = "Movie_Columns">
+            <h1>{title}</h1>
+            <div className="Movie_Genres">
+              {genres.map( (genre, index) => <MovieGenre genre={genre} key={index} />)}
+            </div>
+            <p className="Movie_Synopsis">
+              {synopsis}
+            </p>
+          </div>
         </div>
   )
 }
 
 // dumb component
-function MoviePoster({poster}){
+function MoviePoster({poster,alt}){
   return(
-    <img src={poster} alt='Movie Poster' />
+    <img src={poster} alt={alt} title={alt} className="Movie_Poster" />
   )
 }
 
+function MovieGenre({genre}){
+  return (
+    <span className="Movie_Genre">{genre} </span>
+  ) 
+}
+
+
 Movie.propTypes = {
   title : propTypes.string.isRequired,
-  poster : propTypes.string.isRequired
+  poster : propTypes.string.isRequired,
+  genres : propTypes.array.isRequired,
+  synopsis : propTypes.string.isRequired
+
 }
 MoviePoster.propTypes = {
-  poster : propTypes.string.isRequired
+  poster : propTypes.string.isRequired,
+  alt:propTypes.string.isRequired
+}
+MoviePoster.propTypes = {
+  genre:propTypes.string.isRequired
 }
 
 
