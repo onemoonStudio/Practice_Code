8eca6bf6100346cc32378867d5d5cdbb5b012080
onemoon <onemoonstudio@gmail.com>
2018-01-15 18:36:30 +0900

    redux example

---
+++ redux_udemy/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title : 'harry potter' , pages:101},
+        {title : 'one thing',pages:123},
+        {title : 'perfect study' ,pages:12 },
+        {title : 'emperor of ring' ,pages:1011}
+    ];
+}
