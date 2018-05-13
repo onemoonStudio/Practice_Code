59c2d69ee000ccb926761487430e8a49313a2eb1
onemoon <onemoonstudio@gmail.com>
2018-05-13 14:33:48 +0900

    remove : react and redux

--- redux_udemy/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function(){
-    return [
-        {title : 'harry potter' , pages:101},
-        {title : 'one thing',pages:123},
-        {title : 'perfect study' ,pages:12 },
-        {title : 'emperor of ring' ,pages:1011}
-    ];
-}
