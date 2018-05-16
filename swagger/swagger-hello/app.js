64e3bc89e9a810c4654bda5f7e794cf9ce62db54
onemoon <onemoonstudio@gmail.com>
2018-05-17 01:49:13 +0900

    feat : about swagger modules
    basic use for swagger
    and link swagger ui ( '/docs' )

---
+++ swagger/swagger-hello/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+var app = express();
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+const SwaggerUi = require("swagger-tools/middleware/swagger-ui");
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // add swagger-ui ('/docs')
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
+});
